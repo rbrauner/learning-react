d827fcf926bf2ad580399f3b3df8b0a3d8cad99d
rbrauner <rafalbrauner@gmail.com>
2022-05-03 10:26:54 +0200

    https://www.taniarascia.com/getting-started-with-react/\#submitting-form-data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import {Component} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ],
+        characters: [],
     }
 
     render() {
@@ -29,6 +13,7 @@ class App extends Component {
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         );
     }
@@ -42,6 +27,12 @@ class App extends Component {
             })
         })
 
+    }
+
+    handleSubmit = (character) => {
+        this.setState({
+            characters: [...this.state.characters, character]
+        })
     }
 }
 
