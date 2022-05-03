0914c900f0f167cfec79682c434679be14d8359b
rbrauner <rafalbrauner@gmail.com>
2022-05-03 10:47:47 +0200

    Clear workspace

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Learning - React
 
-## Docs
-
-- [react](docs/react.md)
-
 ## Sources
 
 - https://www.taniarascia.com/getting-started-with-react/
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import App from "./Api";
-import "./index.css";
-
-ReactDOM.render(<App/>, document.getElementById("root"));
